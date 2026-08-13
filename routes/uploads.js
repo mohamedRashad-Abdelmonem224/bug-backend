5d8197966a09386aff307c34962112e6b04720fc
Mohammed Rashad <mo.rashad991@gmail.com>
2026-08-13 14:39:14 +0300

    Update uploads.js

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,15 +1,15 @@
-const fs = require('fs')
-const uploadDir = path.join(__dirname, '..', 'public', 'uploads')
-fs.mkdirSync(uploadDir, { recursive: true })
-
 const router = require('express').Router()
 const multer = require('multer')
 const path = require('path')
+const fs = require('fs')
 const uploadController = require('../controllers/uploadController')
+
+const uploadDir = path.join(__dirname, '..', 'public', 'uploads')
+fs.mkdirSync(uploadDir, { recursive: true })
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '..', 'public', 'uploads'))
+    cb(null, uploadDir)
   },
   filename: function (req, file, cb) {
     const unique = Date.now() + '-' + Math.round(Math.random() * 1E9)
